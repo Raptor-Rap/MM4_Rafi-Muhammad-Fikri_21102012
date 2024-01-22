3a94520d97a80f1b9383fdf5e67f24e363d45f3b
Raptor-Rap <rmfikri42@gmail.com>
2024-01-22 20:13:23 +0700

    chore: Install admin LTE

--- practice_laravel/laravel/app/Http/Controllers/StudentController.php
+++ practice_laravel/laravel/app/Http/Controllers/StudentController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 use App\Models\Student;
 
 use Illuminate\Http\Request;
+use File;
 
 class StudentController extends Controller
 {
@@ -27,6 +28,12 @@ class StudentController extends Controller
         $mahasiswa->gender = $validateData['jenis_kelamin'];
         $mahasiswa->departement = $validateData['jurusan'];
         $mahasiswa->address = $validateData['alamat'];
+        if ($request->hasFile('image')) {
+            $extFile = $request->image->getClientOriginalExtension();
+            $namaFile = 'user-' . time() . "." . $extFile;
+            $path = $request->image->move('assets/images', $namaFile);
+            $mahasiswa->image = $path;
+        }
         $mahasiswa->save();
         $request->session()->flash('pesan', 'Penambahan data berhasil');
         return redirect()->route('student.index');
@@ -58,12 +65,19 @@ class StudentController extends Controller
             'jenis_kelamin' => 'required|in:P,L',
             'jurusan' => 'required',
             'alamat' => '',
+            'image' => 'required|file|image|max:1000',
         ]);
         $student->nim = $validateData['nim'];
         $student->name = $validateData['nama'];
         $student->gender = $validateData['jenis_kelamin'];
         $student->departement = $validateData['jurusan'];
         $student->address = $validateData['alamat'];
+        if ($request->hasFile('image')) {
+            $extFile = $request->image->getClientOriginalExtension();
+            $namaFile = 'user-' . time() . "." . $extFile;
+            $path = $request->image->move('assets/images', $namaFile);
+            $student->image = $path;
+        }
         $student->save();
         $request->session()->flash('pesan', 'Perubahan data berhasil');
         return redirect()->route('student.show', ['student' => $student->id]);
@@ -71,6 +85,7 @@ class StudentController extends Controller
 
     public function destroy(Request $request, Student $student)
     {
+        File::delete($student->image);
         $student->delete();
         $request->session()->flash('pesan', 'Hapus data berhasil');
         return redirect()->route('student.index');
